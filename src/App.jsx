3caf46163e9c49024c9eb6d7a99d8c1f2fb5b0fe
Matt McCullough <matthewmccullough@rockethomes.com>
2022-01-06 16:03:29 -0500

    Emotion component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
+import SectionEmotion from "./SectionEmotion";
 import "./App.css";
 
 function App() {
-	return (
-		<div id="body">
-			<h1>Devvin on CSS in JS</h1>
-		</div>
-	);
+  return (
+    <div id="body">
+      <h1>Devvin on CSS in JS</h1>
+      <SectionEmotion />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/SectionEmotion.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function SectionEmotion() {
+  return <div>Sweet Emotion</div>;
+}
+
+export default SectionEmotion;
