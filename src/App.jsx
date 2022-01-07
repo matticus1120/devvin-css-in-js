becec9ac41b6ff9fefa1b58baf97c3a006b23615
Matt McCullough <matthewmccullough@rockethomes.com>
2022-01-07 13:06:10 -0500

    Components in

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,13 @@ import Chakra from "./Chakra";
 import "./App.css";
 
 function App() {
-	return (
-		<div id="body">
-			<h1>Devvin on CSS in JS</h1>
-			<Emotion />
-			<Chakra />
-		</div>
-	);
+  return (
+    <div id="body">
+      <h1>Devvin on CSS in JS</h1>
+      <Emotion />
+      <Chakra />
+    </div>
+  );
 }
 
 export default App;
