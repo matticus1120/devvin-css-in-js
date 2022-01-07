118bc9d28f843b7537745daaaa8485af71641773
Matticus <matt@matticus.ca>
2022-01-07 09:33:37 -0500

    Chakra installed, files created

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import SectionEmotion from "./SectionEmotion";
+import Chakra from "./Chakra";
 import "./App.css";
 
 function App() {
-  return (
-    <div id="body">
-      <h1>Devvin on CSS in JS</h1>
-      <SectionEmotion />
-    </div>
-  );
+	return (
+		<div id="body">
+			<h1>Devvin on CSS in JS</h1>
+			<SectionEmotion />
+			<Chakra />
+		</div>
+	);
 }
 
 export default App;
