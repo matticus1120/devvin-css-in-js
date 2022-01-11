a314eba839c2b3d370d6a9212621303cd0f43381
Matticus <matt@matticus.ca>
2022-01-10 21:16:12 -0500

    Pretty

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,12 @@ import Emotion from "./Emotion";
 import "./App.css";
 
 function App() {
-  return (
-    <div id="body">
-      <h1>Devvin on CSS in JS</h1>
-      <Emotion />
-    </div>
-  );
+	return (
+		<div id="body">
+			<h1>Devvin on CSS in JS</h1>
+			<Emotion />
+		</div>
+	);
 }
 
 export default App;
