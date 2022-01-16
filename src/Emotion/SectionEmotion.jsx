3572a0e4c72d7473853f52e3cf3054948d70bdd0
Matticus <matt@matticus.ca>
2022-01-16 16:12:48 -0500

    More devvin

--- src/Emotion/SectionEmotion.jsx
+++ src/Emotion/SectionEmotion.jsx
@@ -1,43 +1,41 @@
 /** @jsxImportSource @emotion/react */
 import React, { useEffect } from "react";
 import { css, useTheme } from "@emotion/react";
-import { tryObjectAssign } from "./helpers";
+import { tryTaggedTemplateLiterals } from "./helpers";
 import { Button, Container } from "./components";
 
 function SectionEmotion(props) {
-  const theme = useTheme();
-  useEffect(() => {
-    tryObjectAssign();
-  }, []);
-  return (
-    <Container>
-      <h2>Sweet Emotion</h2>
-      <div>
-        <h3
-          css={{
-            backgroundColor: theme.colors.silver,
-            color: [theme.colors.white, theme.colors.red, theme.colors.blue],
-            padding: theme.space[4],
-          }}
-        >
-          Sweet h3 bro
-        </h3>
-        <Button
-          // bg={theme.colors.red}
-          bg="red"
-          sx={{
-            backgroundColor: "hot-pink",
-          }}
-          isDisabled={false}
-          onClick={() => {
-            alert("You clicked me");
-          }}
-        >
-          Click Me
-        </Button>
-      </div>
-    </Container>
-  );
+	const theme = useTheme();
+	useEffect(() => {
+		tryTaggedTemplateLiterals();
+	}, []);
+	return (
+		<Container>
+			<h2>Sweet Emotion</h2>
+			<div>
+				<h3
+					css={{
+						backgroundColor: theme.colors.silver,
+						color: [theme.colors.white, theme.colors.red, theme.colors.blue],
+						padding: theme.space[4],
+					}}>
+					Sweet h3 bro
+				</h3>
+				<Button
+					// bg={theme.colors.red}
+					bg="red"
+					sx={{
+						backgroundColor: "hot-pink",
+					}}
+					isDisabled={false}
+					onClick={() => {
+						alert("You clicked me");
+					}}>
+					Click Me
+				</Button>
+			</div>
+		</Container>
+	);
 }
 
 export default SectionEmotion;
--- src/Emotion/helpers.js
+++ src/Emotion/helpers.js
@@ -1,55 +1,90 @@
 export function useCSSObject(props) {
-  const { m, mt, p, pb, color, bg, fontSize, ...rest } = props;
-  return [
-    {
-      ...(m && { margin: m }),
-      ...(mt && { marginTop: mt }),
-      ...(p && { padding: p }),
-      ...(pb && { paddingBottom: pb }),
-      ...(color && { color: color }),
-      ...(bg && { background: bg }),
-      ...(fontSize && { fontSize: fontSize }),
-    },
-    rest,
-  ];
+	const { m, mt, p, pb, color, bg, fontSize, ...rest } = props;
+	return [
+		{
+			...(m && { margin: m }),
+			...(mt && { marginTop: mt }),
+			...(p && { padding: p }),
+			...(pb && { paddingBottom: pb }),
+			...(color && { color: color }),
+			...(bg && { background: bg }),
+			...(fontSize && { fontSize: fontSize }),
+		},
+		rest,
+	];
 }
 
 export function tryObjectAssign() {
-  let person = {
-    firstName: "Matt",
-    middleName: "Joseph",
-    lastName: "McCullough",
-    hairColor: "Blondle",
-    hairStyle: "Pretty sweet",
-  };
+	let person = {
+		firstName: "Matt",
+		middleName: "Joseph",
+		lastName: "McCullough",
+		hairColor: "Blondle",
+		hairStyle: "Pretty sweet",
+	};
 
-  const faves = {
-    faveColor: "Mauve",
-    faveMovie: "Syncronic",
-    faveBand: "War On Drugs",
-    faveGuitar: "Gibson",
-  };
+	const faves = {
+		faveColor: "Mauve",
+		faveMovie: "Syncronic",
+		faveBand: "War On Drugs",
+		faveGuitar: "Gibson",
+	};
 
-  const newPerson = Object.assign({ ...person }, faves);
-  console.log("person", person);
-  console.log("faves", faves);
-  console.log("newPerson", newPerson);
+	const newPerson = Object.assign({ ...person }, faves);
 
-  Object.defineProperty(person, "hoodie", {
-    value: "He is wearing it.",
-    configurable: true,
-    enumerable: false, // when false, "hoodie" will not show up in the below for() loop
-    writable: true,
-  });
+	Object.defineProperty(person, "hoodie", {
+		value: "He is wearing it.",
+		configurable: true,
+		enumerable: false,
+		writable: true,
+	});
 
-  const firstNameDescriptor = Object.getOwnPropertyDescriptor(
-    person,
-    "firstName"
-  );
+	const firstNameDescriptor = Object.getOwnPropertyDescriptor(
+		person,
+		"firstName"
+	);
 
-  const hoodieDescriptor = Object.getOwnPropertyDescriptor(person, "hoodie");
+	const hoodieDescriptor = Object.getOwnPropertyDescriptor(person, "hoodie");
+}
+function Person() {
+	(this.name = "John"), (this.age = 23);
+}
 
-  for (let prop in person) {
-    // console.log("prop", prop);
-  }
+export function tryOutProtoTypes() {
+	// creating objects
+	const person1 = new Person();
+	const person2 = new Person();
+
+	// adding property to constructor function
+	// Person.prototype.gender = "male";
+	Person.prototype.gender = "male";
+
+	// prototype value of Person
+	console.log(Person.prototype);
+
+	// inheriting the property from prototype
+	console.log(person1.gender);
+	console.log(person2.gender);
+}
+
+function curryOne(table) {
+	return (mediaStand) => (stool) =>
+		`Media stand is ${mediaStand}, and table is ${table}, stool is ${stool}`;
+}
+
+export function tryOutCurry() {
+	const results = curryOne("brown")("white")("black");
+	console.log("results: ", results);
+}
+
+let greetings = "Hi";
+let name = "Techsith";
+let age = 35;
+
+function transform(staticTags, ...tags) {
+	console.log(staticTags); // ["", " my name is ", " and I am ", ""]
+	console.log(tags); //["Hi", "Techsith", 35]
+}
+export function tryTaggedTemplateLiterals() {
+	transform`${greetings} my name is ${name} and I am ${age}`;
 }
